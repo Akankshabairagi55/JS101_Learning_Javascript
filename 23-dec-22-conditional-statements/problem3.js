30131631ade26cf94f89ae4709edf3f309501e31
Akankshabairagi55 <you@example.com>
2023-01-02 23:51:39 +0530

    conditional statements

---
+++ 23-dec-22-conditional-statements/problem3.js
@@ -0,0 +1,8 @@
+let a=8;
+let b=5;
+if(a>b){
+  console.log("Greater");
+}
+else if(a==b){
+  console.log("Both Equal");
+}
---
+++ 23-dec-22-conditional-statements/problem4.js
@@ -0,0 +1,22 @@
+let original_username = "Akanksha";
+let original_pass = "Ab123";
+
+
+let username = "Akanksha";
+let pass = "Ab123";
+
+  
+if(original_username==username ){
+ 
+  if(original_pass==  pass){
+    console.log("logged in");
+  }
+        else{
+                console.log("Wrong password");
+        }
+}
+else{
+  console.log("Wrong username ");
+}
+  
+
