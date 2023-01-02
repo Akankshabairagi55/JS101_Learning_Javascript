e3346c5eb8436fcd64b160e990821f8f57abdfc0
Akankshabairagi55 <you@example.com>
2023-01-03 00:03:43 +0530

    while loops

---
+++ 26-dec-22-while-loops/p3.js
@@ -0,0 +1,12 @@
+let a = 0;
+let sum = 0;
+while (a <= 10) {
+        if (a % 3 == 0) {
+                 sum = sum + a;
+                
+
+        }
+       
+        a++;
+}
+ console.log(sum);
---
+++ 26-dec-22-while-loops/p4.js
@@ -0,0 +1,12 @@
+let a=0;
+let sum=0;
+let count =0;
+while(a<=10){
+        if(a%2==0){
+                sum=sum+a;
+                count++;
+        }
+        a++;
+        
+}
+console.log(sum/count);
